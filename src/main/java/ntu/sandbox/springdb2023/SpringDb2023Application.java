0bdc2df57fa8dc2e4c7696435b37ac7af7da00f5
Artem_Oharkov <aoharkov@kse.org.ua>
2023-11-09 19:49:26 +0200

    initialize project with https://start.spring.io/

---
+++ src/main/java/ntu/sandbox/springdb2023/SpringDb2023Application.java
@@ -0,0 +1,13 @@
+package ntu.sandbox.springdb2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDb2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDb2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/ntu/sandbox/springdb2023/SpringDb2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package ntu.sandbox.springdb2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDb2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
